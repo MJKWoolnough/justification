76764ebc01c65f11166f78a938217fda40e7dacd
Michael Woolnough <michael.woolnough@gmail.com>
2022-11-07 15:31:23 +0000

    implemented routing logic

--- schema.go
+++ schema.go
@@ -20,8 +20,60 @@ func (s *SchemaMap) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		s.handleSchema(w, r)
 	} else if strings.HasPrefix(r.URL.Path, "/validate/") || r.URL.Path == "/validate" {
 		s.handleValidate(w, r)
+	} else if r.URL.Path == "/" {
+		// list endpoints
+	} else {
+		http.Error(w, "", http.StatusNotFound)
 	}
 }
 
 func (s *SchemaMap) handleSchema(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/schema")
+	switch r.Method {
+	case http.MethodGet:
+		if id == "" || id == "/" {
+			// list schema
+		} else {
+			s.serveSchema(w, id[1:])
+		}
+	case http.MethodPost:
+		if id == "" || id == "/" {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+		} else {
+			s.uploadSchema(w, r, id[1:])
+		}
+	case http.MethodOptions:
+	default:
+		http.Error(w, "", http.StatusMethodNotAllowed)
+	}
+}
+
+func (s *SchemaMap) handleValidate(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/schema")
+	switch r.Method {
+	case http.MethodGet:
+		if id == "" || id == "/" {
+			// list schema
+		} else {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+		}
+	case http.MethodPost:
+		if id == "" || id == "/" {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+		} else {
+			s.validateJSON(w, r, id[1:])
+		}
+	case http.MethodOptions:
+	default:
+		http.Error(w, "", http.StatusMethodNotAllowed)
+	}
+}
+
+func (s *SchemaMap) serveSchema(w http.ResponseWriter, id string) {
+}
+
+func (s *SchemaMap) uploadSchema(w http.ResponseWriter, r *http.Request, id string) {
+}
+
+func (s *SchemaMap) validateJSON(w http.ResponseWriter, r *http.Request, id string) {
 }
