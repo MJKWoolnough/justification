97353cd70d635e9231f51e70e630bfed8d556b9f
Michael Woolnough <michael.woolnough@gmail.com>
2022-11-07 15:06:19 +0000

    added basic routing

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"os/signal"
 	"path"
 	"path/filepath"
+	"strings"
 	"sync"
 
 	jsonschema "github.com/santhosh-tekuri/jsonschema/v5"
@@ -23,6 +24,17 @@ type SchemaMap struct {
 }
 
 func (s *SchemaMap) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.URL.Path, "/schema/") || r.URL.Path == "/schema" {
+		s.handleSchema(w, r)
+	} else if strings.HasPrefix(r.URL.Path, "/validate/") || r.URL.Path == "/validate" {
+		s.handleValidate(w, r)
+	}
+}
+
+func (s *SchemaMap) handleSchema(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *SchemaMap) handleValidate(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
